882994c2e039662b32c9f753efb740784023af5b
Advansed <atlasov.n.r@gmail.com>
2025-08-06 10:46:43 +0900

    11

--- src/components/Lics/components/List/LicsList.tsx
+++ src/components/Lics/components/List/LicsList.tsx
@@ -35,45 +35,37 @@ const LicsList: React.FC<LicsListProps> = ({
     <div className={styles.container}>
       <h2 className={styles.title}>Лицевые счета</h2>
       
-      <div className={styles.tableWrapper}>
-        <table className={styles.table}>
-          <thead>
-            <tr>
-              <th>Лицевой счет</th>
-              <th>ФИО</th>
-              <th>Адрес</th>
-              <th>Участок</th>
-              <th>Долг</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((lic) => {
-              const totalDebt = getTotalDebt(lic.debts);
-              const debtStatus = getDebtStatus(lic.debts);
+      <div className={styles.itemsContainer}>
+        {data.map((lic) => {
+          const totalDebt = getTotalDebt(lic.debts);
+          const debtStatus = getDebtStatus(lic.debts);
+          
+          return (
+            <div 
+              key={lic.id}
+              className={`${styles.item} ${styles[`debt${debtStatus.charAt(0).toUpperCase() + debtStatus.slice(1)}`]}`}
+              onClick={() => onLicClick(lic)}
+            >
+              <div className={styles.itemHeader}>
+                <div className={styles.code}>{lic.code}</div>
+                <div className={styles.debtContainer}>
+                  <span className={styles.debtSum}>
+                    {formatSum(totalDebt)}
+                  </span>
+                  {debtStatus === 'positive' && (
+                    <span className={styles.debtIndicator}>●</span>
+                  )}
+                </div>
+              </div>
               
-              return (
-                <tr 
-                  key={lic.id}
-                  className={`${styles.row} ${styles[`debt${debtStatus.charAt(0).toUpperCase() + debtStatus.slice(1)}`]}`}
-                  onClick={() => onLicClick(lic)}
-                >
-                  <td className={styles.code}>{lic.code}</td>
-                  <td className={styles.name}>{lic.name}</td>
-                  <td className={styles.address}>{formatAddress(lic.address_go)}</td>
-                  <td className={styles.plot}>{lic.plot}</td>
-                  <td className={styles.debt}>
-                    <span className={styles.debtSum}>
-                      {formatSum(totalDebt)}
-                    </span>
-                    {debtStatus === 'positive' && (
-                      <span className={styles.debtIndicator}>●</span>
-                    )}
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+              <div className={styles.itemBody}>
+                <div className={styles.name}>{lic.name}</div>
+                <div className={styles.address}>{formatAddress(lic.address_go)}</div>
+                <div className={styles.plot}>Участок: {lic.plot}</div>
+              </div>
+            </div>
+          );
+        })}
       </div>
       
       {data.length === 0 && (
