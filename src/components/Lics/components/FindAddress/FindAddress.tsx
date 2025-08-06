4814bb967819bb54ab08e6092e71c31ad61488b6
Advansed <atlasov.n.r@gmail.com>
2025-08-06 10:32:34 +0900

    11

--- src/components/Lics/components/FindAddress/FindAddress.tsx
+++ src/components/Lics/components/FindAddress/FindAddress.tsx
@@ -15,10 +15,9 @@ import {
     IonPopover
 } from '@ionic/react';
 import { locationOutline, ellipsisHorizontal, chevronDownOutline, saveOutline } from 'ionicons/icons';
-import { useDaData } from '../../dadata-component/useDaData';
-import { ConfidenceLevel, StandardizedAddress } from '../../dadata-component/types';
-import { useToast } from '../../Toast/useToast';
 import './FindAddress.css';
+import { ConfidenceLevel, StandardizedAddress, useDaData } from '../../../dadata-component';
+import { useToast } from '../../../Toast/useToast';
 
 interface LicsProps {
     initialAddress?: string;
